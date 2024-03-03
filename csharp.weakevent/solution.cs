b50853de1d24a6e5eb95fec5fb5e42d0cac356a4
asm-jaime <asm.jaime@gmail.com>
2024-03-04 06:56:42 +1000

    weak event: attempt to demonstrate how weak event even looks like

---
+++ csharp.weakevent/solution.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace csharpweakevent;
+
+public class EventSource
+{
+    public event EventHandler<EventArgs> Event = delegate { };
+
+    public void Raise()
+    {
+        Event(this, EventArgs.Empty);
+    }
+}
+
+
+public class EventListener
+{
+    public int Counter { get; set; }
+    EventSource eventSourceObject;
+
+    public EventListener(EventSource source)
+    {
+        eventSourceObject = source;
+        eventSourceObject.Event += MyHandler;
+    }
+
+    private void MyHandler(object source, EventArgs args)
+    {
+        Counter++;
+        Console.WriteLine("MyHandler received event " + Counter);
+    }
+
+    public void UnRegisterEvent()
+    {
+        eventSourceObject.Event -= MyHandler;
+    }
+}
+
+static class Executor
+{
+    public static void ExecuteEventSequence()
+    {
+        EventSource source = new EventSource();
+        EventListener listener = new EventListener(source);
+
+        Console.WriteLine("Raise event first time");
+        Console.Read();
+        source.Raise();
+
+        //Set listener to null and call GC to release the  
+        //listener object from memory.  
+
+        Console.WriteLine("\nSet listener to null and call GC");
+        Console.Read();
+        //developer forget to unregister the event               
+
+        //listener.UnRegisterEvent();  
+        listener = null;
+        GC.Collect();
+        Console.WriteLine("\nRaise event 2nd time");
+        Console.Read();
+        source.Raise();
+        Console.ReadKey();
+    }
+}
---
+++ csharp.weakevent/test.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace csharpweakevent;
+
+
+[TestFixture]
+public class SolutionTests
+{
+    [Test]
+    public void TestWeakEvents()
+    {
+        // Arrange
+        // Act
+        Executor.ExecuteEventSequence();
+        // Assert
+    }
+}
+
