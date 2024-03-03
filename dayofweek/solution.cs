c23dee7d5297185190729da85b7b6d99cbb699a4
asm-jaime <asm.jaime@gmail.com>
2024-03-04 07:01:13 +1000

    day of week: add

---
+++ dayofweek/solution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace dayofweek;
+
+public static class ClosestDay
+{
+    public static DayOfWeek GetNextClosestDay(this DayOfWeek value, IList<DayOfWeek> listOfDaysOfWeek) =>
+        listOfDaysOfWeek
+            .Select(day => new
+            {
+                day = day,
+                gap = value <= day ? day - value : (int)DayOfWeek.Saturday + (day - value),
+            })
+            .OrderBy(dayGap => dayGap.gap).First().day;
+}
---
+++ dayofweek/test.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace dayofweek;
+
+
+[TestFixture]
+public class DayOfWeekExtensionMethodsTests
+{
+    [Test]
+    public void Test1()
+    {
+        // arrange
+        var listOfDays = new List<DayOfWeek> { DayOfWeek.Saturday, DayOfWeek.Sunday };
+
+        // act
+        var actualResult = DayOfWeek.Monday.GetNextClosestDay(listOfDays);
+
+        // assert
+        Assert.AreEqual(DayOfWeek.Saturday, actualResult);
+    }
+
+    [Test]
+    public void Test2()
+    {
+        // arrange
+        var listOfDays = new List<DayOfWeek> { DayOfWeek.Saturday, DayOfWeek.Sunday, DayOfWeek.Wednesday };
+
+        // act
+        var actualResult = DayOfWeek.Sunday.GetNextClosestDay(listOfDays);
+
+        // assert
+        Assert.AreEqual(DayOfWeek.Sunday, actualResult);
+    }
+
+    [Test]
+    public void Test3()
+    {
+        // arrange
+        var listOfDays = new List<DayOfWeek> { DayOfWeek.Thursday, DayOfWeek.Sunday };
+
+        // act
+        var actualResult = DayOfWeek.Friday.GetNextClosestDay(listOfDays);
+
+        // assert
+        Assert.AreEqual(DayOfWeek.Sunday, actualResult);
+    }
+}
+
+
