e9e2a562343e1937fec3f8d407f85fc9e6f8e702
asm-jaime <asm.jaime@gmail.com>
2024-03-03 15:46:07 +1000

    bitcount: add bit counter

---
+++ bitcount/solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace bitcount;
+
+public class Solution
+{
+    public int CountBits(int number)
+    {
+        int result = 0;
+        int bits = (int)Math.Ceiling(Math.Log2(number));
+        for(int bit = 0; bit <= bits; ++bit)
+        {
+            if(((byte)(number >> bit) & 1) == 1)
+            {
+                result++;
+            }
+        }
+        return result;
+    }
+}
---
+++ bitcount/test.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace bitcount;
+
+[TestFixture]
+public class BitCountingTest
+{
+    [TestCase(0, 0)]
+    [TestCase(4, 1)]
+    [TestCase(7, 3)]
+    [TestCase(9, 2)]
+    [TestCase(10, 2)]
+    public void CountBits(int input, int expectedCount)
+    {
+        var solution = new Solution();
+        Assert.AreEqual(expectedCount, solution.CountBits(input));
+    }
+}
