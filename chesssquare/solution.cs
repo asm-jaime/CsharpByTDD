a29ce27808ed4e08ef1c88f0b6af906a1f1bdcba
asm-jaime <asm.jaime@gmail.com>
2024-03-04 06:54:05 +1000

    chesssquare: add solution

---
+++ chesssquare/solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace chesssquare;
+
+public class Solution
+{
+    public int SUMA002378(int n) => (n - 1) * (n) * (n + 1) / 3;
+
+    public int StableCrossSize(int x, int y) => Math.Abs(x - y) + 1;
+
+    public int SquaresVerticalHorizontal(int x, int y) => x * y * (x + y - 2);
+
+    public int SquaresOneCross(int x, int y)
+    {
+        var unstableCross = SUMA002378(Math.Min(x, y) - 1);
+        var stableCross = StableCrossSize(x, y) * Math.Min(x, y) * (Math.Min(x, y) - 1);
+        return 2 * unstableCross + stableCross;
+    }
+
+    public int ChessboardSquaresUnderQueenAttack(int x, int y)
+    {
+        return SquaresVerticalHorizontal(x, y) + 2 * SquaresOneCross(x, y);
+    }
+}
---
+++ chesssquare/test.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+
+namespace chesssquare;
+
+
+[TestFixture]
+public class SolutionTests
+{
+    private static Solution _solution;
+
+    [SetUp]
+    private static void Initialize()
+    {
+        _solution = new Solution();
+    }
+
+    [Test]
+    public void BasicTest()
+    {
+        Assert.AreEqual(320, _solution.ChessboardSquaresUnderQueenAttack(5, 5));
+    }
+
+    [Test]
+    public void TestA002378()
+    {
+        Assert.AreEqual(0, _solution.SUMA002378(1));
+        Assert.AreEqual(2, _solution.SUMA002378(2));
+        Assert.AreEqual(8, _solution.SUMA002378(3));
+    }
+
+    [Test]
+    public void TestVH()
+    {
+        Assert.AreEqual(8, _solution.SquaresVerticalHorizontal(2, 2));
+        Assert.AreEqual(18,_solution.SquaresVerticalHorizontal(2, 3));
+    }
+
+    [Test]
+    public void TestStableCross()
+    {
+        Assert.AreEqual(1, _solution.StableCrossSize(2, 2));
+        Assert.AreEqual(2, _solution.StableCrossSize(3, 2));
+        Assert.AreEqual(2, _solution.StableCrossSize(2, 3));
+        Assert.AreEqual(1, _solution.StableCrossSize(3, 3));
+        Assert.AreEqual(1, _solution.StableCrossSize(5, 5));
+        Assert.AreEqual(3, _solution.StableCrossSize(5, 3));
+    }
+}
+
