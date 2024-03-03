188a257401b0ede7f02d8e4b4ab77f7d08841bf0
asm-jaime <asm.jaime@gmail.com>
2024-03-03 15:57:28 +1000

    bitsort: add solution

---
+++ bitsort/solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace bitsort;
+
+public class Solution
+{
+    const int DefaultBase = 2;
+    const string SelectEightDigitsPattern = @"\d{8}";
+
+    public string SortBits(string bits)
+    {
+        return Regex
+            .Matches(bits, SelectEightDigitsPattern)
+            .Select(digit => Convert.ToInt32(digit.ToString(), DefaultBase))
+            .OrderBy(digit => digit)
+            .Aggregate("", (currentDigit, nextDigit) => $"{currentDigit}{nextDigit}");
+    }
+}
---
+++ bitsort/test.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace bitsort;
+
+
+[TestFixture]
+public class SolutionTests
+{
+    [Test]
+    public void ShouldMakeSortedNumber()
+    {
+        var bitsorts = new Solution();
+        bitsorts.SortBits("000000100000011101010101000000101101000100000001").Should().Be("122785209");
+    }
+}
+
