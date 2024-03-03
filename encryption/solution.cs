55778ec95a0298eb0bb801bc3d5d9c2616276c17
asm-jaime <asm.jaime@gmail.com>
2024-03-04 07:02:07 +1000

    encryption: basic crypt encrypt problem solution

---
+++ encryption/solution.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+
+namespace encryption;
+
+public class Encryption
+{
+    private static string Mix(string text)
+    {
+        StringBuilder left = new StringBuilder();
+        StringBuilder right = new StringBuilder();
+        for(int i = 0; i < text.Length; ++i)
+        {
+            if(i % 2 == 0)
+            {
+                right.Append(text[i]);
+            }
+            else
+            {
+                left.Append(text[i]);
+            }
+        }
+        return $"{left}{right}";
+    }
+
+    private static string Unmix(string cryptedText)
+    {
+        int length = cryptedText.Length / 2;
+
+        string left = cryptedText.Substring(0, length);
+        string right = cryptedText.Substring(length);
+        StringBuilder result = new StringBuilder();
+        for(int i = 0; i < right.Length; ++i)
+        {
+            result.Append(right[i]);
+            if(i == left.Length) continue;
+            result.Append(left[i]);
+        }
+
+        return result.ToString();
+    }
+
+    private static string RepeatFunction(Func<string, string> repeatFunc, string text, int time)
+    {
+        var currentText = text;
+        for(int i = 0; i < time; ++i)
+        {
+            currentText = repeatFunc(currentText);
+        }
+
+        return currentText;
+
+    }
+
+    public static string Encrypt(string text, int n)
+    {
+        return RepeatFunction(Mix, text, n);
+    }
+
+    public static string Decrypt(string text, int n)
+    {
+        return RepeatFunction(Unmix, text, n);
+    }
+}
---
+++ encryption/test.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+
+namespace encryption;
+
+
+[TestFixture]
+public class SolutionTest
+{
+    [Test]
+    public void EncryptExampleTests()
+    {
+        Assert.AreEqual("This is a test!", Encryption.Encrypt("This is a test!", 0));
+        Assert.AreEqual("hsi  etTi sats!", Encryption.Encrypt("This is a test!", 1));
+        Assert.AreEqual("s eT ashi tist!", Encryption.Encrypt("This is a test!", 2));
+        Assert.AreEqual(" Tah itse sits!", Encryption.Encrypt("This is a test!", 3));
+        Assert.AreEqual("This is a test!", Encryption.Encrypt("This is a test!", 4));
+        Assert.AreEqual("This is a test!", Encryption.Encrypt("This is a test!", -1));
+        Assert.AreEqual("hskt svr neetn!Ti aai eyitrsig", Encryption.Encrypt("This kata is very interesting!", 1));
+    }
+
+    [Test]
+    public void DecryptExampleTests()
+    {
+        Assert.AreEqual("This is a test!", Encryption.Decrypt("This is a test!", 0));
+        Assert.AreEqual("This is a test!", Encryption.Decrypt("hsi  etTi sats!", 1));
+        Assert.AreEqual("This is a test!", Encryption.Decrypt("s eT ashi tist!", 2));
+        Assert.AreEqual("This is a test!", Encryption.Decrypt(" Tah itse sits!", 3));
+        Assert.AreEqual("This is a test!", Encryption.Decrypt("This is a test!", 4));
+        Assert.AreEqual("This is a test!", Encryption.Decrypt("This is a test!", -1));
+        Assert.AreEqual("This kata is very interesting!", Encryption.Decrypt("hskt svr neetn!Ti aai eyitrsig", 1));
+    }
+
+    [Test]
+    public void EmptyTests()
+    {
+        Assert.AreEqual("", Encryption.Encrypt("", 0));
+        Assert.AreEqual("", Encryption.Decrypt("", 0));
+    }
+
+    [Test]
+    public void NullTests()
+    {
+        Assert.AreEqual(null, Encryption.Encrypt(null, 0));
+        Assert.AreEqual(null, Encryption.Decrypt(null, 0));
+    }
+}
+
