9d697e5087dc38ed07c89a44743effe012d76c0c
asm-jaime <asm.jaime@gmail.com>
2024-03-04 07:03:29 +1000

    extremums: add solution where you can find all extremums for an array

---
+++ extremums/program.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace extremums;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    }
+}
---
+++ extremums/solution.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace extremums;
+
+public class PickPeaks
+{
+    public static Dictionary<string, List<int>> GetPeaks(int[] arr)
+    {
+        var skipPlateau = arr
+          .Select((element, index) => new { Element = element, Index = index })
+          .Where((element, index) =>
+          {
+              if(index == 0) return true;
+              if(index == arr.Length - 1) return true;
+              if(element.Element == arr[index - 1]) return false;
+              return true;
+          }).ToArray();
+
+        var results = skipPlateau.Where((element, index) =>
+          {
+              if(index == 0) return false;
+              if(index == skipPlateau.Length - 1) return false;
+              if(
+                  element.Element > skipPlateau[index - 1].Element &&
+                  element.Element > skipPlateau[index + 1].Element
+              ) return true;
+
+              return false;
+          });
+
+        var pos = results.Select(element => element.Index).ToList();
+        var peaks = results.Select(element => element.Element).ToList();
+        var result = new Dictionary<string, List<int>>() { { "pos", pos }, { "peaks", peaks } };
+        return result;
+    }
+}
