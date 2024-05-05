8e9db2b14ed1a4e4734f8def8152a1ba5d7ea954
asm-jaime <asm.jaime@gmail.com>
2024-05-06 08:10:18 +1000

    fizzbuzz: move to tasks category

---
+++ fizzbuzz/solution.cs
@@ -0,0 +1,53 @@
+using System;
+
+namespace fizzbuzz;
+
+public class Solution
+{
+    public void PrintFizzBuzz(int num)
+    {
+        for(var i = 1; i <= num; i++)
+        {
+            if(i % 3 is 0 && i % 5 is 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if(i % 3 is 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if(i % 5 is 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+    public void PrintFizzBuzzWithUseOneOutput(int num)
+    {
+        for(var i = 1; i <= num; i++)
+        {
+            var result = string.Empty;
+
+            if(i % 3 is 0)
+            {
+                result = "Fizz";
+            }
+            if(i % 5 is 0)
+            {
+                result = $"{result}Buzz";
+            }
+
+            if(string.IsNullOrEmpty(result))
+            {
+                result = i.ToString();
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
