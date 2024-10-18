7907b6b7570b2d550205d24804ed596ec69c5373
asm-jaime <asm.jaime@gmail.com>
2024-10-18 19:26:58 +1000

    add async finite machine example and spy++

---
+++ csharp.ILSPY/solution.cs
@@ -0,0 +1,9 @@
+namespace csharpILSPY;
+
+class Solution
+{
+    internal int Calculate(int a, int b)
+    {
+        return a + b;
+    }
+}
---
+++ csharp.async.finite-machine/program.cs
@@ -0,0 +1,162 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace csharpasyncfinitemachine;
+
+class Program
+{
+    /*
+    [CompilerGenerated]
+    private struct <Add>d__0 : IAsyncStateMachine
+    {
+        public int <>1__state;
+
+        public AsyncTaskMethodBuilder<int> <>t__builder;
+
+        public int a;
+
+        public int b;
+
+        private TaskAwaiter <>u__1;
+
+        private void MoveNext()
+        {
+            int num = <>1__state;
+            int result;
+            try
+            {
+                TaskAwaiter awaiter;
+                if (num != 0)
+                {
+                    awaiter = Task.Delay(1000).GetAwaiter();
+                    if (!awaiter.IsCompleted)
+                    {
+                        num = (<>1__state = 0);
+                        <>u__1 = awaiter;
+                        <>t__builder.AwaitUnsafeOnCompleted(ref awaiter, ref this);
+                        return;
+                    }
+                }
+                else
+                {
+                    awaiter = <>u__1;
+                    <>u__1 = default(TaskAwaiter);
+                    num = (<>1__state = -1);
+                }
+                awaiter.GetResult();
+                result = a + b;
+            }
+            catch (Exception exception)
+            {
+                <>1__state = -2;
+                <>t__builder.SetException(exception);
+                return;
+            }
+            <>1__state = -2;
+            <>t__builder.SetResult(result);
+        }
+
+        void IAsyncStateMachine.MoveNext()
+        {
+            //ILSpy generated this explicit interface implementation from .override directive in MoveNext
+            this.MoveNext();
+        }
+
+        [DebuggerHidden]
+        private void SetStateMachine(IAsyncStateMachine stateMachine)
+        {
+            <>t__builder.SetStateMachine(stateMachine);
+        }
+
+        void IAsyncStateMachine.SetStateMachine(IAsyncStateMachine stateMachine)
+        {
+            //ILSpy generated this explicit interface implementation from .override directive in SetStateMachine
+            this.SetStateMachine(stateMachine);
+        }
+    }
+    */
+
+    private static async Task<int> Add(int a, int b)
+    {
+        await Task.Delay(1000);
+        return a + b;
+    }
+
+    /*
+    [CompilerGenerated]
+    private struct <Main>d__1 : IAsyncStateMachine
+    {
+        public int <>1__state;
+
+        public AsyncTaskMethodBuilder <>t__builder;
+
+        private TaskAwaiter<int> <>u__1;
+
+        private void MoveNext()
+        {
+            int num = <>1__state;
+            try
+            {
+                TaskAwaiter<int> awaiter;
+                if (num != 0)
+                {
+                    int b = 2;
+                    awaiter = Add(1, b).GetAwaiter();
+                    if (!awaiter.IsCompleted)
+                    {
+                        num = (<>1__state = 0);
+                        <>u__1 = awaiter;
+                        <>t__builder.AwaitUnsafeOnCompleted(ref awaiter, ref this);
+                        return;
+                    }
+                }
+                else
+                {
+                    awaiter = <>u__1;
+                    <>u__1 = default(TaskAwaiter<int>);
+                    num = (<>1__state = -1);
+                }
+                awaiter.GetResult();
+                Console.ReadLine();
+            }
+            catch (Exception exception)
+            {
+                <>1__state = -2;
+                <>t__builder.SetException(exception);
+                return;
+            }
+            <>1__state = -2;
+            <>t__builder.SetResult();
+        }
+
+        void IAsyncStateMachine.MoveNext()
+        {
+            //ILSpy generated this explicit interface implementation from .override directive in MoveNext
+            this.MoveNext();
+        }
+
+        [DebuggerHidden]
+        private void SetStateMachine(IAsyncStateMachine stateMachine)
+        {
+            <>t__builder.SetStateMachine(stateMachine);
+        }
+
+        void IAsyncStateMachine.SetStateMachine(IAsyncStateMachine stateMachine)
+        {
+            //ILSpy generated this explicit interface implementation from .override directive in SetStateMachine
+            this.SetStateMachine(stateMachine);
+        }
+    }
+    */
+
+    static async Task Main(string[] args)
+    {
+        var a = 1;
+        var b = 2;
+
+        await Add(a, b);
+
+        Console.ReadLine();
+    }
+}
